f491f2852148cd43494e02fcb2da09f10cb949f4
Marshal10 <marshalmaleane123@gmail.com>
2024-06-26 21:15:22 +0530

    Structure of app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,57 @@
 export default function App() {
   return (
     <div className="app">
-      <h1>Hello React</h1>
+      <h1>Classy Weather</h1>
+      <div>
+        <input type="text"></input>
+      </div>
+      <div className="output">
+        <h2>Weather Placename</h2>
+        <ul className="weather">
+          <li className="day">
+            <span>☁️</span>
+            <p>Today</p>
+            <p>
+              27-<strong>31</strong>
+            </p>
+          </li>
+          <li className="day">
+            <span>☁️</span>
+            <p>Today</p>
+            <p>
+              27-<strong>31</strong>
+            </p>
+          </li>
+          <li className="day">
+            <span>☁️</span>
+            <p>Today</p>
+            <p>
+              27-<strong>31</strong>
+            </p>
+          </li>
+          <li className="day">
+            <span>☁️</span>
+            <p>Today</p>
+            <p>
+              27-<strong>31</strong>
+            </p>
+          </li>
+          <li className="day">
+            <span>☁️</span>
+            <p>Today</p>
+            <p>
+              27-<strong>31</strong>
+            </p>
+          </li>
+          <li className="day">
+            <span>☁️</span>
+            <p>Today</p>
+            <p>
+              27-<strong>31</strong>
+            </p>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
